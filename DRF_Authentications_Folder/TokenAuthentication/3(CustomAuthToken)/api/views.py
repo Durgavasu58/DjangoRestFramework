7191f9a8a002fcdf353ea5d8eaeac23f3b683e67
Durgavasu58 <durgavasu58@gmail.com>
2021-10-11 12:45:56 +0530

    TokenAuthentication

---
+++ DRF_Authentications_Folder/TokenAuthentication/3(CustomAuthToken)/api/views.py
@@ -0,0 +1,23 @@
+
+from .models import Student
+from .serializer import StudentSerializer
+from rest_framework import  viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+
+# ===============================================================
+
+# http POST http://127.0.0.1:8000/gettoken/ username="user" password="User@123"
+#   -->   pip install httpie Check urls.py
+
+# ===============================================================
+
+# use when local authentications are to be made & check settings.py file
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    # authentication_classes = [SessionAuthentication]
+    # permission_classes = [IsAuthenticated] # Open the url incognito broser u can login with username and password
+    
