8db3c6a59f3205c2eed120b8288d4d85d388f73c
Durgavasu58 <durgavasu58@gmail.com>
2021-10-11 15:23:16 +0530

    Toketnauthentications

---
+++ DRF_Authentications_Permissions/TokenAuthentication/4(Signals)/Signals/urls.py
@@ -0,0 +1,24 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+
+#   -->   pip install httpie
+
+
+# Creating Router Object
+router = DefaultRouter()
+
+#Register Views with Router
+router.register('studentapi',views.StudentModelViewSet,basename='student')
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path('auth/',include('rest_framework.urls',namespace='rest_framework')),
+    
+    
+]
--- DRF_Authentications_Permissions/TokenAuthentication/5(Token_httpie)/api/views.py
+++ DRF_Authentications_Permissions/TokenAuthentication/5(Token_httpie)/api/views.py
@@ -3,21 +3,14 @@ from .models import Student
 from .serializer import StudentSerializer
 from rest_framework import  viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 
-
-
-# ===============================================================
-
-# http POST http://127.0.0.1:8000/gettoken/ username="user" password="User@123"
-#   -->   pip install httpie Check urls.py
-
-# ===============================================================
-
+ 
 # use when local authentications are to be made & check settings.py file
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    # authentication_classes = [SessionAuthentication]
+    authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated] # Open the url incognito broser u can login with username and password
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
